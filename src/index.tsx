76a316766f44bd1f2695fd9c150df1334654376c
Andrii <andrew.belous1412@gmail.com>
2023-07-05 18:58:27 +0300

    add Preview Component

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,15 @@ import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 const App = () => {
+	const [code, setCode] = useState('');
 	const [input, setInput] = useState('');
 
 	const ref = useRef<any>();
-	const iframe = useRef<any>();
 
 	const startService = async () => {
 		ref.current = await esbuild.startService({
@@ -28,8 +29,6 @@ const App = () => {
 	const onClick = async () => {
 		if (!ref.current) return;
 
-		iframe.current.srcdoc = html;
-
 		const result = await ref.current.build({
 			entryPoints: ['index.js'],
 			bundle: true,
@@ -41,39 +40,16 @@ const App = () => {
 			},
 		});
 
-		iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+		setCode(result.outputFiles[0].text);
 	};
-
-	const html = `
-		<html>
-			<head></head>	
-			<body>
-				<div id='root'></div>
-				<script>
-					window.addEventListener('message', (event) => {
-						try {
-						  	eval(event.data)
-						} catch (err) {
-							const root = document.querySelector('#root');
-							
-							root.innerHTML = '<div style="color: red;"><h4>Runtime Error!</h4>' + err + '</div>';
-							
-							console.log(err)
-						}
-					}, false)
-				</script>
-			</body>
-		</html>
-	`;
 
 	return (
 		<div>
 			<CodeEditor initialValue="const a = 1;" onChange={(value) => setInput(value)} />
-			<textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
 			<div>
 				<button onClick={onClick}>Submit</button>
 			</div>
-			<iframe title="preview" ref={iframe} srcDoc={html} sandbox="allow-scripts"></iframe>
+			<Preview code={code} />
 		</div>
 	);
 };
