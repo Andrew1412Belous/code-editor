d98ed7dec7c538a4bf5965ae62617ae1aaae79f5
Andrii <andrew.belous1412@gmail.com>
2023-07-05 18:41:03 +0300

    ** add highlight jsx (downgrade react-scripts <5)

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,6 +2,11 @@ import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
 import React, { useRef } from 'react';
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
+
+import './code-editor.css';
+import './syntax.css';
 
 interface CodeEditorProps {
 	initialValue: string;
@@ -20,24 +25,42 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
 		});
 
 		monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+
+		const highlighter = new Highlighter(
+			//@ts-ignore
+			window.monaco,
+			codeShift,
+			monacoEditor,
+		);
+
+		highlighter.highLightOnDidChangeModelContent(
+			() => {},
+			() => {},
+			undefined,
+			() => {},
+		);
 	};
 
 	const onFormatCLick = () => {
 		const unformatted = editorRef.current.getModel().getValue();
-		const formatted = prettier.format(unformatted, {
-			parser: 'babel',
-			plugins: [parser],
-			useTabs: false,
-			semi: true,
-			singleQuote: true,
-		});
+		const formatted = prettier
+			.format(unformatted, {
+				parser: 'babel',
+				plugins: [parser],
+				useTabs: false,
+				semi: true,
+				singleQuote: true,
+			})
+			.replace(/\n$/, '');
 
 		editorRef.current.setValue(formatted);
 	};
 
 	return (
-		<div>
-			<button onClick={onFormatCLick}>Format</button>
+		<div className="editor-wrapper">
+			<button onClick={onFormatCLick} className="button button-format is-primary is-small">
+				Format
+			</button>
 			<MonacoEditor
 				editorDidMount={onEditorDidMount}
 				value={initialValue}
