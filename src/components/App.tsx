f6d2ba4f461757265424cdb3770db70be4767159
Andrii <andrew.belous1412@gmail.com>
2023-07-07 10:35:00 +0300

    add text editor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import CodeCell from './code-cell/Code-cell';
+import TextEditor from './text-editor/Text-editor';
 
 const App = () => {
 	return (
 		<div>
-			<CodeCell />
+			<TextEditor />
+			{/*<CodeCell />*/}
 		</div>
 	);
 };
