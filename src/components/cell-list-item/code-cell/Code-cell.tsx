b47d50c8d761707488e5ad1ed83b2e0fe9c76f29
Andrii <andrew.belous1412@gmail.com>
2023-07-10 16:02:08 +0300

    adjusting reducer state

--- src/components/cell-list-item/code-cell/Code-cell.tsx
+++ src/components/cell-list-item/code-cell/Code-cell.tsx
@@ -3,31 +3,33 @@ import bundle from '../../../bundler';
 import CodeEditor from './code-editor/Code-editor';
 import Preview from './preview/Preview';
 import Resizable from './resizable/Resizable';
-import { Cell } from '../../../store';
+import { Cell, createBundle } from '../../../store';
 import { useCellsActions } from '../../../hooks/useActions/useCellsActions';
+import { useTypedDispatch } from '../../../hooks/useTypedDispatch';
+import { useTypedSelector } from '../../../hooks/useTypedSelector';
 
 interface CodeCellProps {
 	cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }: CodeCellProps) => {
-	const [code, setCode] = useState('');
-	const [err, setErr] = useState('');
-
 	const { updateCell } = useCellsActions();
+	const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+	const dispatch = useTypedDispatch();
 
 	useEffect(() => {
-		const timer = setTimeout(async () => {
-			const result = await bundle(cell.content);
+		if (!bundle) {
+			dispatch(createBundle({ cellId: cell.id, input: cell.content }));
+		}
 
-			setCode(result.code);
-			setErr(result.err);
+		const timer = setTimeout(async () => {
+			dispatch(createBundle({ cellId: cell.id, input: cell.content }));
 		}, 750);
 
 		return () => {
 			clearTimeout(timer);
 		};
-	}, [cell.content]);
+	}, [cell.id, cell.content]);
 
 	return (
 		<Resizable direction="vertical">
@@ -38,7 +40,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }: CodeCellProps) => {
 						onChange={(value) => updateCell({ id: cell.id, content: value })}
 					/>
 				</Resizable>
-				<Preview code={code} err={err} />
+				{bundle && <Preview code={bundle.code} err={bundle.err} />}
 			</div>
 		</Resizable>
 	);
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,3 +3,4 @@ export * from './types/cell';
 export * from './types/bundle';
 export * from './slices/cellsSlice';
 export * from './slices/bundleSlice';
+export * from './thunks/bundlesThunks';
