70c44f4dfecbfe94314b84b09360bfe03f401552
Andrii <andrew.belous1412@gmail.com>
2023-07-11 11:30:19 +0300

    minor fix

--- src/components/cell-list/Cell-list.tsx
+++ src/components/cell-list/Cell-list.tsx
@@ -3,6 +3,8 @@ import { useTypedSelector } from '../../hooks/useTypedSelector';
 import CellListItem from '../cell-list-item/Cell-list-item';
 import AddCell from '../add-cell/Add-cell';
 
+import './cell-list.css';
+
 const CellList: React.FC = () => {
 	const cellsState = useTypedSelector((state) => state.cells);
 	const cells = cellsState.order.map((id) => cellsState.data[id]);
@@ -15,7 +17,7 @@ const CellList: React.FC = () => {
 	));
 
 	return (
-		<div>
+		<div className="cell-list">
 			<AddCell forceVisible={cells.length === 0} previousCellId={null} />
 			{renderedCells}
 		</div>
